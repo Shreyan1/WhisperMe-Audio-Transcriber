fe1e037c63f504c1735e1ec54816580257a8fdb6
Basu Ray, Shreyan <shreyan.gogol@gmail.com>
2024-02-11 07:15:05 +0530

    launchers added

---
+++ run.sh
@@ -0,0 +1,12 @@
+# Created by Shreyan Basu Ray [Github - https://github.com/Shreyan1] @2024
+# Support me at - https://github.com/sponsors/Shreyan1
+
+# Built for the mission - "Accessible AI Education and Technology for all"
+# Project Repo = https://github.com/Shreyan1/WhisperMe-Audio-Transcriber
+# WhisperMe
+
+#!/bin/sh
+CWD="$(pwd)"
+python3 src/GUI.py
+# for Linux and MacOS based systems
+
